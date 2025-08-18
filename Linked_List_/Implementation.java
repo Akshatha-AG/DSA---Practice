487b6291ecb562be6f9ea8732fe2f27486b56acc
Akshatha-AG <152718862+Akshatha-AG@users.noreply.github.com>
2025-08-18 23:11:45 +0530

    Add files via upload

---
+++ Linked_List_/Implementation.java
@@ -0,0 +1,124 @@
+public class Implementation{
+    Node head;
+    
+
+    class Node{
+        int data;
+        Node next;
+
+        Node(int d){
+            data=d;
+        }
+    }
+
+    //Adding Elements
+    public void addFirst(int data){
+        Node newNode=new Node(data);
+        if(head==null){
+            head=newNode;
+            return;
+        }
+        newNode.next = head;
+        head=newNode;
+    }
+
+    public void addLast(int data){
+        Node newNode=new Node(data);
+        if(head==null){
+            head= newNode;
+            return;
+        }
+
+        Node cuNode = head;
+
+        while(cuNode.next !=null){
+            cuNode=cuNode.next;
+        }
+        cuNode.next=newNode;
+        
+    }
+
+
+    public void addmid(int data, int idx){
+        Node newNode = new Node(data);
+        if(head==null){
+            head=newNode;
+            return;
+        }
+        if(idx==0){
+            addFirst(data);
+            return;
+        }
+
+        Node cuNode=head;
+        for ( int i=0; i<idx-1; i++){
+            cuNode=cuNode.next;
+        }
+        newNode.next=cuNode.next;
+        cuNode.next=newNode;
+    }
+
+    //Deleting Elements
+    public void deleteFirst(){
+        if(head==null){
+            System.out.println("Empty list");
+            return;
+        }
+
+        head = head.next;
+    }
+
+    public void deleteLast(){
+        if(head==null){
+            System.out.println("Empty list");
+            return;
+        }
+
+        Node secondLast = head;
+        Node last = head.next;
+
+        while(last.next!=null){
+            secondLast=secondLast.next;
+            last=last.next;
+        }
+        secondLast.next=null;
+
+    }
+
+    public void deleteMid(int idx){
+        if(head == null){
+            System.out.println("Empty list");
+            return;
+        }
+        Node cuNode = head;
+        for(int i=0; i<idx-1; i++){
+            cuNode = cuNode.next;
+        }
+        cuNode.next = cuNode.next.next;
+    }
+
+    //Printing Linked list
+    public void printList(){
+        Node cuNode=head;
+
+        while(cuNode.next!=null){
+            System.out.print (cuNode.data+" -> ");
+            cuNode=cuNode.next;
+        }
+        System.out.print(cuNode.data);
+    }
+
+    public static void main(String[] args) {
+        Implementation list =new Implementation();
+        list.addFirst(50);
+        list.addFirst(25);
+        list.addLast(75);
+        list.addLast(100);
+        list.addmid(0, 0);
+        list.printList();
+        
+        list.deleteLast();
+        System.out.println();
+        list.printList();
+    }
+}
