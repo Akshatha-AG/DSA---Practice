479cd049079c09906ed0b2043d32821b598acfac
Akshatha-AG <152718862+Akshatha-AG@users.noreply.github.com>
2025-10-03 14:32:15 +0530

    Add files via upload

---
+++ Sorting Techniques/MergeSort.java
@@ -0,0 +1,51 @@
+public class MergeSort{
+    public static void main(String[] args) {
+        int arr[] = {4,3,1,7,9,2,10,56,21,98,64};
+        int n = arr.length;
+        divide(arr, 0, n-1);
+        for(int i : arr){
+            System.out.print(i+" ");
+        }
+    }
+
+    public static void divide(int arr[], int si, int ei){
+        if(si >= ei){
+            return;
+        }
+
+        int mid = si + (ei - si)/2;
+        divide(arr, si, mid);
+        divide(arr, mid+1, ei);
+
+        conquer(arr, si, mid, ei);
+    }
+
+    public static void conquer(int arr[], int si, int mid, int ei){
+
+        int merged[] = new int[ei - si + 1];
+        int idx1 = si;
+        int idx2 = mid+1;
+        int x = 0;
+
+        while(idx1 <= mid && idx2 <= ei){
+            if(arr[idx1] <= arr[idx2]){
+                merged[x++]=arr[idx1++];
+            }else{
+                merged[x++] = arr[idx2++];
+            }
+        }
+
+        while (idx1 <= mid){
+            merged[x++] = arr[idx1++];
+        }
+
+        while(idx2 <= ei){
+            merged[x++] = arr[idx2++];
+        }
+        int j=si;
+        for( int i=0; i<merged.length; i++){
+            arr[j]=merged[i];
+            j++;
+        }
+    }
+}
