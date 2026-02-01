066715710b2f78e2af29ce11945d674496a5598f
Akshatha-AG <152718862+Akshatha-AG@users.noreply.github.com>
2026-02-01 21:11:59 +0530

    Add files via upload

---
+++ Questions _solved/FirstOccurence.java
@@ -0,0 +1,12 @@
+public class FirstOcuurance{
+    public static void main(String[] args) {
+      //Find the index of first occurence of string
+
+      String haystack = "sabbutsad";
+      String needle= "sad";
+
+      System.out.println(haystack.indexOf(needle));
+      
+    }
+}
+
