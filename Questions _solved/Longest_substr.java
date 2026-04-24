e48210664e62867274cc4298b06c0b8c4932d52f
Akshatha-AG <152718862+Akshatha-AG@users.noreply.github.com>
2026-04-24 11:18:54 +0530

    Create Longest_substr.java

---
+++ Questions _solved/Longest_substr.java
@@ -0,0 +1,24 @@
+//Find the longest SubString without repeating characters ( sliding window )
+
+import java.util.*;
+class Solution {
+    public static int lengthOfLongestSubstring(String s) {
+        HashSet<Character> set = new HashSet<>();
+        int left = 0;
+        int max = 0;
+
+        for(int right = 0; right < s.length(); right++){
+            while(set.contains(s.charAt(right))){
+                set.remove(s.charAt(left));
+                left++;
+            }
+            set.add(s.charAt(right));
+            max = Math.max(max, right - left + 1);
+        }
+        return max;
+    }
+  public static void main(String args[]) {
+    String s = "abba";
+    int res = lengthOgLongestSubstring(s);
+  }
+}
